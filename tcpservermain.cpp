4e0a562861d84ea9da39f2117898c99605e7931c
Ponlin02 <ponlin.02@gmail.com>
2025-09-26 13:11:07 +0200

    Established listening

--- tcpservermain.cpp
+++ tcpservermain.cpp
@@ -2,7 +2,11 @@
 #include <string.h>
 #include <stdlib.h>
 /* You will to add includes here */
-
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netdb.h>
+#include "protocol.h"
 
 // Included to get the support library
 #include <calcLib.h>
@@ -13,6 +17,11 @@
 
 
 using namespace std;
+
+bool serverSetup(int &sockfd, char hoststring, char portstring)
+{
+  
+}
 
 
 int main(int argc, char *argv[]){
@@ -48,5 +57,82 @@ int main(int argc, char *argv[]){
   
   printf("TCP server on: %s:%s\n", hoststring,portstring);
 
-  
+  int sockfd;
+
+  //variable that will be filled with data
+  struct addrinfo *res, *pInfo;
+
+  struct addrinfo hints;
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE;
+
+  int addrinfo_status = getaddrinfo(hoststring, portstring, &hints, &res);
+  if(addrinfo_status != 0)
+  {
+    printf("\nERROR: getaddrinfo Failed\n");
+    printf("Returned: %d\n", addrinfo_status);
+    return EXIT_FAILURE;
+  }
+
+  #ifdef DEBUG
+  printf("getaddrinfo Succeded!\n");
+  #endif
+
+  for(pInfo = res; pInfo != NULL; pInfo = pInfo->ai_next)
+  {
+    sockfd = socket(pInfo->ai_family, pInfo->ai_socktype, pInfo->ai_protocol);
+    if(sockfd != -1)
+    {
+      break;
+    }
+    #ifdef DEBUG
+    printf("Socket retry");
+    #endif
+  }
+
+  if(sockfd == -1)
+  {
+    printf("\nERROR: Socket creation Failed\n");
+    printf("Returned: %d\n", sockfd);
+    return EXIT_FAILURE;
+  }
+
+  #ifdef DEBUG
+  printf("Socket creation Succeded!\n");
+  #endif
+
+  int opt = 1;
+  setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+  int bind_status = bind(sockfd, pInfo->ai_addr, pInfo->ai_addrlen);
+  if(bind_status == -1)
+  {
+    printf("\nERROR: Bind Failed\n");
+    printf("Returned: %d\n", bind_status);
+    close(sockfd);
+    return EXIT_FAILURE;
+  }
+
+  #ifdef DEBUG
+  printf("Bind Succeded!\n");
+  #endif
+
+  int listen_status = listen(sockfd, SOMAXCONN);
+  if(listen_status == -1)
+  {
+    printf("\nERROR: Listen Failed\n");
+    printf("Returned: %d\n", listen_status);
+    close(sockfd);
+    return EXIT_FAILURE;
+  }
+
+  #ifdef DEBUG
+  printf("Listen Succeded!\n");
+  #endif
+
+  freeaddrinfo(res);
+  close(sockfd);
+  return 0;
 }
