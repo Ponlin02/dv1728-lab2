dbc02a5800def93f3926ac0adb1b449e63e4d9d3
Ponlin02 <ponlin.02@gmail.com>
2025-09-30 16:01:49 +0200

    udp server is hard had to refactor it many times

--- udpservermain.cpp
+++ udpservermain.cpp
@@ -7,6 +7,7 @@
 #include <unistd.h>
 #include <netdb.h>
 //#include <signal.h>
+#include <time.h>
 #include "protocol.h"
 
 
@@ -16,9 +17,51 @@
 // Enable if you want debugging to be printed, see examble below.
 // Alternative, pass CFLAGS=-DDEBUG to make, make CFLAGS=-DDEBUG
 #define DEBUG
-
+#define MAXCLIENTS 10
 
 using namespace std;
+
+struct clientInfo
+{
+  struct sockaddr_storage addr;
+  socklen_t addr_len;
+  time_t last_seen;
+
+  bool active;
+};
+
+//Function that tries to find a client if it exists in the table, 
+//if not try to put it in the table. Returns index or "-1" for ERROR
+int client_lockup(struct clientInfo *table, struct sockaddr_storage *addr, socklen_t addr_len)
+{
+  //Try to find an existing client
+  for(int i = 0; i < MAXCLIENTS; i++)
+  {
+    if(table[i].active &&
+      table[i].addr_len == addr_len &&
+      memcmp(&table[i].addr, addr, addr_len) == 0)
+      {
+        return i; //client index
+      }
+  }
+
+  //New client, put the client in table
+  for(int i = 0; i < MAXCLIENTS; i++)
+  {
+    if(!table[i].active)
+    {
+      table[i].addr = *addr;
+      table[i].addr_len = addr_len;
+      table[i].last_seen = time(NULL);
+
+      table[i].active = true;
+      return i; //client index
+    }
+  }
+
+  //Client didnt exist, and table was full, error = -1
+  return -1;
+}
 
 bool serverSetup(int &sockfd, char *hoststring, char *portstring)
 {
@@ -83,7 +126,6 @@ bool serverSetup(int &sockfd, char *hoststring, char *portstring)
   return true;
 }
 
-
 int main(int argc, char *argv[]){
   if (argc < 2) {
     fprintf(stderr, "Usage: %s protocol://server:port/path.\n", argv[0]);
@@ -118,10 +160,53 @@ int main(int argc, char *argv[]){
   printf("UDP Server on: %s:%s\n", hoststring,portstring);
 
   int sockfd;
+  fd_set readfds;
   bool setup_status = serverSetup(sockfd, hoststring, portstring);
   if(!setup_status)
   {
     return EXIT_FAILURE;
+  }
+
+  struct clientInfo client_table[MAXCLIENTS];
+
+  while(1)
+  {
+    FD_ZERO(&readfds);
+    FD_SET(sockfd, &readfds);
+
+    int select_status = select(sockfd + 1, &readfds, NULL, NULL, NULL);
+    if(select_status == -1)
+    {
+      printf("ERROR: Select error\n");
+      continue;
+    }
+
+    if(FD_ISSET(sockfd, &readfds))
+    {
+      char recv_buffer[1024];
+      struct sockaddr_storage client_addr;
+      socklen_t addr_len = sizeof(client_addr);
+
+      int bytes_recieved = recvfrom(sockfd, recv_buffer, sizeof(recv_buffer) - 1, 0, (struct sockaddr*)&client_addr, &addr_len);
+      recv_buffer[bytes_recieved] = '\0';
+      if(select_status == -1)
+      {
+        printf("ERROR: Recieve from error\n");
+        continue;
+      }
+      
+      int index = client_lockup(client_table, &client_addr, addr_len);
+      if(index == -1)
+      {
+        printf("ERROR: Too many clients\n");
+        continue;
+      }
+
+      #ifdef DEBUG
+      printf("Client Index = %d\n", index);
+      //printf("Step: %d\n", );
+      #endif
+    }
   }
 
   close(sockfd);
